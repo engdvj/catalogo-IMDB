b335c667b304545444363957d62b89c9135e87e3
engdvj <eng.dvj@gmail.com>
2024-02-07 14:19:26 -0300

    Alterações no Javadoc

--- src/br/com/adatech/projetos/catalogoIMDB/model/ModelDiretor.java
+++ src/br/com/adatech/projetos/catalogoIMDB/model/ModelDiretor.java
@@ -3,7 +3,7 @@ package br.com.adatech.projetos.catalogoIMDB.model;
 /**
  * Classe que representa um Diretor de filme
  * Contém construtores, parâmetros, além de getters e setters
- * @Pessoa - Classe abstrata mãe que possui atributos e métodos próprios
+ * @ModelPessoa - Classe abstrata mãe que possui atributos e métodos próprios
  */
 public class ModelDiretor extends ModelPessoa {
 }
