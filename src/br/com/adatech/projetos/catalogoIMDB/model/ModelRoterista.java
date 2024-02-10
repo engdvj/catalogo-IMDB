96af26cfab19b93a6c025195355126e7d039bdce
Pedro Antonio Souza <pedroantoniiodm@gmail.com>
2024-02-10 13:05:06 -0300

    Nova Classe
    Analisando o catálogo da IMBd percebi que outra pessoa importe são os roteristas, então indroduzi um model e service deles já seguindo o escopo das outras classes presentes

---
+++ src/br/com/adatech/projetos/catalogoIMDB/model/ModelRoterista.java
@@ -0,0 +1,36 @@
+package br.com.adatech.projetos.catalogoIMDB.model;
+
+import java.time.LocalDate;
+
+/**
+ * Classe que representa um Roteirista de filme
+ * Contém construtores, parâmetros, além de getters e setters
+ * @ModelPessoa - Classe abstrata mãe que possui atributos e métodos próprios
+ */
+public class ModelRoterista extends ModelPessoa {
+
+    int quantidadeDeRoteirosEscritos;
+    String area;
+
+    public ModelRoterista(String nome, String cpf, LocalDate dataDeNascimento, int quantidadeDeRoteirosEscritos, String area) {
+        super(nome, cpf, dataDeNascimento);
+        this.quantidadeDeRoteirosEscritos = quantidadeDeRoteirosEscritos;
+        this.area = area;
+    }
+
+    public int getQuantidadeDeRoteirosEscritos() {
+        return quantidadeDeRoteirosEscritos;
+    }
+
+    public void setQuantidadeDeRoteirosEscritos(int quantidadeDeRoteirosEscritos) {
+        this.quantidadeDeRoteirosEscritos = quantidadeDeRoteirosEscritos;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
---
+++ src/br/com/adatech/projetos/catalogoIMDB/service/ServiceRoterista.java
@@ -0,0 +1,70 @@
+package br.com.adatech.projetos.catalogoIMDB.service;
+
+import br.com.adatech.projetos.catalogoIMDB.model.ModelRoterista;
+import br.com.adatech.projetos.catalogoIMDB.model.ModelFilme;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Classe que gerencia as operações relacionadas a roteristas no sistema de catálogo de filmes.
+ * Esta classe oferece funcionalidades para adicionar, editar, remover e listar atores,
+ * além de associar e desassociar filmes cadastrados.
+ */
+public class ServiceRoterista {
+
+    /**
+     * Hashmap que armazena os roteristas e seus respectivos filmes.
+     * A chave do mapa é uma String representando o nome do ator.
+     */
+    private static HashMap<String, List<ModelFilme>> roteristasMap = new HashMap<>();
+
+    /**
+     * Adiciona um roterista ao sistema.
+     *
+     * @param roterista O objeto ModelAtor a ser adicionado.
+     */
+    public void adicionarRoterista(ModelRoterista roterista) {}
+
+    /**
+     * Edita os detalhes de um roterista existente no sistema.
+     *
+     * @param roterista O objeto ModelRoterista com as informações atualizadas.
+     */
+    public void editarAtor(ModelRoterista roterista) {}
+
+    /**
+     * Remove um roterista do sistema.
+     *
+     * @param roterista O objeto ModelRoterista a ser removido.
+     */
+    public void removerAtor(ModelRoterista roterista) {}
+
+    /**
+     * Lista todos os roteristas cadastrados no sistema.
+     */
+    public void listarRoteristas() {}
+
+    /**
+     * Fornece os dados de um roterista específico.
+     *
+     * @param roterista O objeto ModelRoterista cujos dados são necessários.
+     */
+    public void dadosAtor(ModelRoterista roterista) {}
+
+    /**
+     * Associa um filme a um roterista.
+     *
+     * @param roterista O objeto ModelRoterista ao qual o filme será associado.
+     * @param filme O objeto ModelFilme a ser associado ao roterista.
+     */
+    public void associarFilme(ModelRoterista roterista, ModelFilme filme) {}
+
+    /**
+     * Desassocia um filme de um roterista.
+     *
+     * @param roterista O objeto ModelRoterista do qual o filme será desassociado.
+     * @param filme O objeto ModelFilme a ser desassociado do diretor.
+     */
+    public void desassociarFilme(ModelRoterista roterista, ModelFilme filme) {}
+}
